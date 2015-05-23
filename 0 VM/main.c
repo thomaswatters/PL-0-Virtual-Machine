04e38eeb9bfc9564c7a7d8fa78d8519fdc30da44
Darian Smalley <darian@Darians-MacBook-Pro.local>
2015-05-23 12:11:08 -0400

    Initial Commit

---
+++ 0 VM/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  0 VM
+//
+//  Created by Darian Smalley on 5/23/15.
+//  Copyright (c) 2015 Darian Smalley. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
