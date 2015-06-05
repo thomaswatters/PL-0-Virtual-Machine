3ff5d36fd7deff872d2514ca930fd8f4144f5ce7
Darian <darian.smalley@gmail.com>
2015-06-05 10:44:17 -0400

    added fillInputStream to get input from file

--- 0 VM/lexi-analyzer.c
+++ 0 VM/lexi-analyzer.c
@@ -12,20 +12,34 @@
 static const int MAX_ID_LEN = 11;
 static const int MAX_NUM_LEN = 5;
 
+void parseInput(char inputStream[]) {
+    //parse input char by char
+}
+
+char* fillInputStream(FILE* file) {
+    static char inputStream[267];
+    int c;
+    int i = 0;
+    
+    while ((c = fgetc(file)) != EOF) {
+        inputStream[i] = c;
+    }
+    
+    return inputStream;
+}
+
 int main(int argc, const char * argv[]) {
     FILE *file;
-    int c;
+    
     
     if (!(file = fopen("input.txt", "r" ))) {
         perror("input.txt");
         exit(1);
     }
     
-    while ((c = fgetc(file)) != EOF) {
-        
-        // now do something with each character, c.
-        
-    }
-    
+    char* inputStream = fillInputStream(file);
+
     fclose(file);
+    
+    parseInput(inputStream);
 }
